06620d4d7f71d8cf1308fbc6672971971c214f82
Rebecca Hom <rebecca.hom001@gmail.com>
2019-02-28 16:47:58 -0800

    updated function for randomizing cards and for creating new boards

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -6,12 +6,22 @@ import ImageCard from './components/ImageCard';
 import pokemon from './pokemon.json';
 import './App.css';
 
+const initialBoard = [];
+const initialSelection = [];
+
+for(let i = 0; i < 12; i++) {
+  initialBoard.push(pokemon[i]);
+  initialSelection.push(i);
+}
+
 class App extends React.Component {
+  
   state = {
     status: "Click an image to begin.",
     highScore: 0,
     score: 0,
-    board: [],
+    selection: initialSelection,
+    board: initialBoard,
     clicked: [],
     pokemon
   };
@@ -21,24 +31,56 @@ class App extends React.Component {
     const clicked = this.state.clicked;
 
     if(clicked.indexOf(id) < 0) {
-      this.setState({score: this.state.score + 1})
-
       clicked.push(id);
-      this.setState({clicked: clicked});
 
-      this.setState({status: "You guessed correctly!"});
+      this.setState({
+        status: "You guessed correctly!",
+        score: this.state.score + 1,
+        clicked: clicked,
+        board: this.updateBoard()
+      });
 
+      // Updates the high score.
       if(this.state.score >= this.state.highScore) this.setState({highScore: this.state.score + 1});
-    } else {
-      this.setState({score: 0});
 
-      this.setState({clicked: []});
-      
-      this.setState({status: "You guessed incorrectly!"});
+      // Checks if the user has won the game.
+      if((this.state.score + 1) % this.state.board.length === 0) {
+        this.setState({
+          status: "You have won this game. The board will now reset.",
+          clicked: [],
+          board: this.newBoard()
+        });
+      }
+    } else {
+      this.setState({
+        status: "You guessed incorrectly!",
+        score: 0,
+        clicked: [],
+        board: this.updateBoard()
+      });
     }
   };
 
-  // 
+  randomizer = (arr) => {
+    let current = arr.length;
+
+    while(0 !== current) {
+      let random = Math.floor(Math.random() * current);
+      current--;
+
+      let temp = arr[current];
+      arr[current] = arr[random];
+      arr[random] = temp;
+    }
+
+    return arr;
+  }
+
+  // Updates the board of displayed images.
+  updateBoard = () => this.randomizer(this.state.board);
+
+  // Sets up a new game.
+  newBoard = () => this.randomizer(this.state.pokemon).slice(0, 12);
 
   render() {
     return (
@@ -46,7 +88,7 @@ class App extends React.Component {
         <Header status={this.state.status} score={this.state.score} highScore={this.state.highScore} />
         <Wrapper>
           <div className="row">
-            {this.state.pokemon.map(mon => <ImageCard
+            {this.state.board.map(mon => <ImageCard
               key={mon.id}
               id={mon.id}
               name={mon.name}
